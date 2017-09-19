644d4a1809ce1228f6b5fc77fa8520240e340b4d
Tim Labas <timlabas@Tims-MacBook-Air.local>
2017-09-19 16:40:57 +0300

    investigating runSerial in promises

--- runSerial.js
+++
@@ -1,9 +0,0 @@
-const runSerial = tasks => {
-    tasks.reduce((promise, task) =>
-        promise.then(result => 
-            task().then(
-                Array.prototype.concat.bind(result)), 
-                Promise.resolve([])
-            )
-    )
-}
