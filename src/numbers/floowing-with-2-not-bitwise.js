cd7fcbcca016858bd6b748d6b206cfc3904d0e9a
tymiurge <tmrlabas@gmail.com>
2017-10-04 23:07:36 +0300

    adding flooring-with-2-not-bitwise.js

---
+++ src/numbers/floowing-with-2-not-bitwise.js
@@ -0,0 +1,25 @@
+/**
+ * ~~ is a double NOT bitwise operator
+ * ~ 5.5 outputs -6
+ * ~ -6 outputs 5
+ * ~~ outputs 5
+ * might be used instead of Math.floor()
+ * However it is claimed that ~~ is a little bit slower of Math.floor()
+ */
+
+ // Tests (just for the sake of interest): 
+ // double NOT bitwise for a negative number
+ // double NOT bitwise for a positive number 
+ // double NOT bitwise for a stringified negavite number i.e '-9'
+ // double NOT bitwise for a stringified positive number i.e '9'
+ // double NOT bitwise for a stringified positive number with '+': '+9'
+ // double NOT bitwise for NaN
+ // double NOT bitwise for undefined 
+ // double NOT bitwise for null
+ // double NOT bitwise for string 
+ // double NOT bitwise for object
+ // double NOT bitwise for boolean
+ // double NOT bitwise for Infinity 
+ // double NOT bitwise for -Infinity 
+ // double NOT bitwise for Hexadecimal
+ // double NOT bitwise for a Number object
